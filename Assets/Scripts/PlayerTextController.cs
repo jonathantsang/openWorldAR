bd60daf47e92f225588af4ee85924cf93396e6dc
jonathantsang <j26tsang@gmail.com>
2018-07-28 01:19:29 -0400

    movement and background

---
+++ Assets/Scripts/PlayerTextController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTextController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TextMesh textBubble = transform.GetChild (1).GetComponent<TextMesh> ();
+		// Turn off by default
+		textBubble.text = ""; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
