3c38a5e722f006559503691f43531156ada90a27
jonathantsang <j26tsang@gmail.com>
2018-07-28 01:42:49 -0400

    tree

---
+++ Assets/Scripts/CollisionCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.collider.CompareTag ("tree")) {
+			print ("tree");
+			// Change to tree scene in AR
+		}
+	}
+}
