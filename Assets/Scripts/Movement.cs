2d53a5f77fe65caddc14957bd1bde887bdc3f95d
jonathantsang <j26tsang@gmail.com>
2018-07-28 01:03:03 -0400

    basic

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour{
+	// Normal Movements Variables
+	private float walkSpeed;
+	private float curSpeed;
+	private float maxSpeed;
+	private Rigidbody2D rb2d;
+
+	void Start()
+	{
+		walkSpeed = 6;
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		curSpeed = walkSpeed;
+		maxSpeed = curSpeed;
+
+		// Move sentences
+		rb2d.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal")* curSpeed, 0.8f),
+			Mathf.Lerp(0, Input.GetAxis("Vertical")* curSpeed, 0.8f));
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # openWorldAR
 adrenalan 2018 
+
+- select skill points
+- traverse world
+- quests 
+- AR examine items/puzzles
+
+- conversation ui
+- parsing text for sentiment:
+- aggressive, neutral, sad, happy
+
